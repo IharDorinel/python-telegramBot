83a7e3d81d92993485b4118112be2aaa92c41d27
Igor Doronin <ihardaronin@gmail.com>
2024-05-08 13:27:18 +0300

    update time points in new

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ def help_message(message):
 
 
 def send_reminders(chat_id):
-    first_rem = "10:00"
-    second_rem = "14:00"
-    last_rem = "18:00"
+    first_rem = "10:30"
+    second_rem = "14:30"
+    last_rem = "18:30"
 
     while True:
         now = datetime.datetime.now().strftime('%H:%M')
